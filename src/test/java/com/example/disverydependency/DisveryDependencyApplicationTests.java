16fca365bd8794170ba23c9cacb1efb332f9aa0e
Elmar465 <elmar465@gmail.com>
2025-02-12 19:35:43 -0500

    Discovery_Dependency

---
+++ src/test/java/com/example/disverydependency/DisveryDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disverydependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisveryDependencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
